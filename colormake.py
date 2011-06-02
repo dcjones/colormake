92da5e9172ea184d0e2de13e9676c3531b0b4a7b
Daniel Jones <dcjones@cs.washington.edu>
2011-06-01 21:09:22 -0700

    a colormake.pl clone

---
+++ colormake.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+
+'''
+A rip-off of colormake.pl by Bjarni R. Einarsson,
+with some small changes and improvements.
+
+Daniel C. Jones <dcjones@cs.washington.edu>
+2011.06.01.20.30
+
+'''
+
+import sys
+import re
+
+# Some useful color codes, see end of file for more.
+#
+col_black =        '\033[30m'
+col_red =          '\033[31m'
+col_green =        '\033[32m'
+col_yellow =       '\033[33m'
+col_blue =         '\033[34m'
+col_magenta =      '\033[35m'
+col_cyan =         '\033[36m'
+col_ltgray =       '\033[37m'
+
+col_norm =	       '\033[00m'
+col_background =   '\033[07m'
+col_brighten =     '\033[01m'
+col_underline =    '\033[04m'
+col_blink = 	   '\033[05m'
+
+
+
+
+# This script works simply by matching regular expressions and wrapping the
+# matches in ANSI terminal codes. 'patterns' is an array of (pattern, prefix)
+# pairs. If a pattern is matched, the prefix is inserted before the line.
+
+# common patterns
+filename_pat = r'[A-z0-9_\/\. ]+'
+gcc_msg_pat  = filename_pat + r':( In |\d+:\d+:)'
+
+patterns = [
+        # make messages
+        (r'^make\[\d+\]:',  col_cyan),
+        (r'^Making all in', col_cyan + col_brighten),
+
+        # make silent rules
+        (r'^  (CC|CXX|CCLD|CXXLD)', col_norm + col_brighten),
+
+        # gcc warnings, errors, etc
+        (gcc_msg_pat + r' warning:', col_yellow),
+        (gcc_msg_pat + r' error:',   col_red),
+        (gcc_msg_pat,                col_yellow + col_brighten),
+
+        # normal
+        (r'', col_norm)]
+
+patterns = [(re.compile(pat[0]),pat[1]) for pat in patterns]
+
+
+for line in iter(sys.stdin.readline, ''):
+    for (pat, form) in patterns:
+        if pat.match(line):
+            sys.stdout.write(form)
+            break
+
+    sys.stdout.write(line)
+    sys.stdout.write(col_norm)
+
+
